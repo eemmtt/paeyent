9c1af06c27753ddf1ddedd7fe94137aa5330cd5e
eemmtt <etmuzio@gmail.com>
2025-04-02 11:17:14 -0400

    init

---
+++ src/main.ts
@@ -0,0 +1,67 @@
+import { Application, FederatedMouseEvent, Graphics } from "pixi.js";
+
+(async () => {
+  // Create a new application
+  const app = new Application();
+  globalThis.__PIXI_APP__ = app; //for chrome extension
+  await app.init({ background: "#cccccc", resizeTo: window });
+  document.getElementById("pixi-container")!.appendChild(app.canvas);  
+
+  app.stage.eventMode = 'static';
+  app.stage.hitArea = app.screen;
+  app.stage.on('mousedown', mouseDown);
+  app.stage.on('mouseup', mouseUp);
+  app.stage.on('mousemove', mouseMove);
+  app.stage.on('rightdown', rightDown);
+
+  const artboard = new Graphics();
+  const inset = 50;
+  const a_height = app.screen.height - (2 * inset);
+  const a_width = app.screen.width - (2 * inset);
+  artboard.rect(inset, inset, a_width, a_height);
+  artboard.fill(0x009999);
+
+  const brush_mask = artboard.clone();
+
+  const brush = new Graphics();
+  
+  app.stage.addChild(brush_mask, artboard, brush);
+  brush.mask = brush_mask;
+ 
+  let isDrawing = false;
+
+  //Event Handlers
+  function mouseDown(event: FederatedMouseEvent) {
+    console.log(event.button);
+    isDrawing = true;
+  };
+
+  function mouseUp() {
+    isDrawing = false;
+  };
+
+  function mouseMove(event: FederatedMouseEvent) {
+    
+    if (isDrawing) {
+      const {x , y} = event.global;
+      brush.fill(0xff0000); // Red brush
+      brush.circle(x, y, 5); // 5px circle as "brush"
+    }
+  };
+
+  function rightDown(event: FederatedMouseEvent) {
+    console.log(event.button);
+  }
+
+  /*
+  // Listen for animate update
+  app.ticker.add((time) => {
+    // Just for fun, let's rotate mr rabbit a little.
+    // * Delta is 1 if running at 100% performance *
+    // * Creates frame-independent transformation *
+    //container.rotation += 0.1 * time.deltaTime;
+    
+  });
+  */
+
+})();
