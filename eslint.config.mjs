65f02619dfcca36debb630ed30acce8cd67cd6ce
eemmtt <etmuzio@gmail.com>
2025-04-11 13:24:07 -0400

    update eslint/no-unused-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,19 @@ export default tseslint.config(
       ecmaVersion: "latest",
       sourceType: "module",
     },
-    rules: {},
-  },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "args": "all",
+        "argsIgnorePattern": "^_",
+        "caughtErrors": "all",
+        "caughtErrorsIgnorePattern": "^_",
+        "destructuredArrayIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }
+    ]
+    },
+  }
 );
